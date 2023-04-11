81e0e5964f06755736cdcd1728a3e46e99947ae2
Robby Lewis <Robbysim37@gmail.com>
2023-04-11 19:39:27 -0400

    canvas work

--- portfolio-website/src/components/canvas/Canvas.js
+++ portfolio-website/src/components/canvas/Canvas.js
@@ -1,13 +1,25 @@
-import React, { useRef,useEffect } from "react"
+import React, { useRef,useEffect,useState } from "react"
 
 const Canvas = (props) => {
+    let [shapes, setShapes] = useState({xStart:100,yStart:0,xLine:100,yLine:100})
+
     const canvasRef = useRef(null)
-    useEffect(() => {
+
+    const animate = () => {        
         const canvas = canvasRef.current
         const c = canvas.getContext("2d")
         c.fillStyle = "#666666"
         c.fillRect(0,0,c.canvas.width,c.canvas.height)
-        console.log(props.headerHeight)
+        c.fillStyle = "#999999"
+        c.fillRect(100,shapes.yStart,100,100)
+        requestAnimationFrame(animate)
+        
+        // setShapes({...shapes,yStart:shapes.yStart++})
+    }
+
+    useEffect(() => {
+
+        animate()
     })
 
     return (
---
+++ portfolio-website/src/components/canvas/CanvasUtils.js
@@ -0,0 +1,7 @@
+const spawnShape = (xPos) => {
+    
+}
+
+module.exports = {
+
+}
