98b45f3bfbd1aa15d414aa312466856398cb7d61
Robby Lewis <Robbysim37@gmail.com>
2023-04-15 14:13:32 -0400

    random walks resets

--- portfolio-website/src/components/pixelGrid/TestGrid.js
+++
@@ -1,9 +0,0 @@
-const TestGrid = (props) => {
-
-    return (
-        <div style={{height:32,width:32}}>
-        {props.block && <img src={`./pixels/testDirt.png`} alt="dirt"></img>}
-        </div>
-    )
-}
-export default TestGrid
